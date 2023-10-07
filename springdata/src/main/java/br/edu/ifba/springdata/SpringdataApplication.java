6bc4471287b3038056d4c08df7989be5c26ad4f0
Luana Braga <luanabragagomes9@gmail.com>
2023-10-07 17:28:23 -0300

    listando e deletando dados

--- springdata/src/main/java/br/edu/ifba/springdata/SpringdataApplication.java
+++ springdata/src/main/java/br/edu/ifba/springdata/SpringdataApplication.java
@@ -46,5 +46,4 @@ public class SpringdataApplication implements CommandLineRunner{
 
 		}
 	}
-	
 }
--- springdata/src/main/java/br/edu/ifba/springdata/service/CrudProfessorService.java
+++ springdata/src/main/java/br/edu/ifba/springdata/service/CrudProfessorService.java
@@ -25,6 +25,8 @@ public class CrudProfessorService{
 			System.out.println("0 - voltar ao menu anterior");
 			System.out.println("1 - cadastrar novo Professor");
             System.out.println("2 - atualizar um Professor");
+            System.out.println("3 - listar os professores cadastrados");
+            System.out.println("4 - delete um professor");
 
 
             int opcao = in.nextInt();
@@ -36,47 +38,70 @@ public class CrudProfessorService{
                 case 2:
                     this.atualizar(in);
                     break;
+                case 3:
+                    this.visulizar(in);
+                    break;
+                case 4:
+                    this.deletar(in);
+                    break;
 				default:
 				 isTrue = false;
 				 break;
 			}
 
         }
-    System.out.println();
- }
-
-private void cadastrar(Scanner in){
-    System.out.println("Digite o nome do professor: ");
-    String nome = in.next(); //ler a proxima string ate achar um enter ou espaço
-
-    System.out.println("Digite o prontuario do professor: ");
-    String prontuario = in.next(); //ler a proxima string ate achar um enter ou espaço
-
-    Professor professor = new Professor(nome, prontuario);
-    this.professorRepository.save(professor);
-    System.out.println("Professor salvo no banco!!\n");
-}
-private void atualizar(Scanner in) {
-    System.out.print("Digite o ID do professor a ser atualizado: ");
-    Long id = in.nextLong();
-
-    Optional<Professor>  optional = this.professorRepository.findById(id);
-    if (optional.isPresent()) {
+        System.out.println();
+    }
+    private void cadastrar(Scanner in){
         System.out.println("Digite o nome do professor: ");
         String nome = in.next(); //ler a proxima string ate achar um enter ou espaço
 
         System.out.println("Digite o prontuario do professor: ");
         String prontuario = in.next(); //ler a proxima string ate achar um enter ou espaço
 
-        Professor professor = optional.get();
-        professor.setNome(nome);
-        professor.setProntuario(prontuario);
-        professorRepository.save(professor); //atualiza o objeto no BD
-        System.out.println("Professor atualizado no banco!!\n");
-    }else{
-        System.out.println("O id do professor informado: "+ id + "é inválido\n");
+        Professor professor = new Professor(nome, prontuario);
+        this.professorRepository.save(professor);
+        System.out.println("Professor salvo no banco!!\n");
     }
+
+    private void atualizar(Scanner in) {
+        System.out.print("Digite o ID do professor a ser atualizado: ");
+        Long id = in.nextLong();
+
+        Optional<Professor>  optional = this.professorRepository.findById(id);
+        if (optional.isPresent()) {
+            System.out.println("Digite o nome do professor: ");
+            String nome = in.next(); //ler a proxima string ate achar um enter ou espaço
+
+            System.out.println("Digite o prontuario do professor: ");
+            String prontuario = in.next(); //ler a proxima string ate achar um enter ou espaço
+
+            Professor professor = optional.get();
+            professor.setNome(nome);
+            professor.setProntuario(prontuario);
+            professorRepository.save(professor); //atualiza o objeto no BD
+            System.out.println("Professor atualizado no banco!!\n");
+        }else{
+            System.out.println("O id do professor informado: "+ id + "é inválido\n");
+        }
 
     }
 
+    private void visulizar(Scanner in) {
+       Iterable<Professor> professores = this.professorRepository.findAll();
+                // for (Professor professor : professores) {
+                //System.out.println(professores);
+                //}
+        professores.forEach(professor -> {
+             System.out.println(professor);
+        });
+       System.out.println();
+    }
+    private void deletar(Scanner in) {
+        System.out.print("Digite o ID do professor a ser deletado: ");
+        Long id = in.nextLong();
+        this.professorRepository.deleteById(id);; //lançara um exception se nao achar um id passado na tabela
+        System.out.println("Professor deletado!!\n");
+
+    }
 }
