6f9050755fdc74aead21a2d4b17e9363a330ee85
Luana Braga <luanabragagomes9@gmail.com>
2023-10-07 17:07:39 -0300

    atualizando registro no BD

--- springdata/src/main/java/br/edu/ifba/springdata/SpringdataApplication.java
+++ springdata/src/main/java/br/edu/ifba/springdata/SpringdataApplication.java
@@ -46,4 +46,5 @@ public class SpringdataApplication implements CommandLineRunner{
 
 		}
 	}
+	
 }
--- springdata/src/main/java/br/edu/ifba/springdata/service/CrudProfessorService.java
+++ springdata/src/main/java/br/edu/ifba/springdata/service/CrudProfessorService.java
@@ -1,5 +1,6 @@
 package br.edu.ifba.springdata.service;
 
+import java.util.Optional;
 import java.util.Scanner;
 import org.springframework.stereotype.Service;
 
@@ -23,6 +24,8 @@ public class CrudProfessorService{
             System.out.println( "\nQual entidade você deseja executar?");
 			System.out.println("0 - voltar ao menu anterior");
 			System.out.println("1 - cadastrar novo Professor");
+            System.out.println("2 - atualizar um Professor");
+
 
             int opcao = in.nextInt();
 
@@ -30,6 +33,9 @@ public class CrudProfessorService{
 				case 1:
                     this.cadastrar(in);
 				    break;
+                case 2:
+                    this.atualizar(in);
+                    break;
 				default:
 				 isTrue = false;
 				 break;
@@ -50,5 +56,27 @@ private void cadastrar(Scanner in){
     this.professorRepository.save(professor);
     System.out.println("Professor salvo no banco!!\n");
 }
+private void atualizar(Scanner in) {
+    System.out.print("Digite o ID do professor a ser atualizado: ");
+    Long id = in.nextLong();
+
+    Optional<Professor>  optional = this.professorRepository.findById(id);
+    if (optional.isPresent()) {
+        System.out.println("Digite o nome do professor: ");
+        String nome = in.next(); //ler a proxima string ate achar um enter ou espaço
+
+        System.out.println("Digite o prontuario do professor: ");
+        String prontuario = in.next(); //ler a proxima string ate achar um enter ou espaço
+
+        Professor professor = optional.get();
+        professor.setNome(nome);
+        professor.setProntuario(prontuario);
+        professorRepository.save(professor); //atualiza o objeto no BD
+        System.out.println("Professor atualizado no banco!!\n");
+    }else{
+        System.out.println("O id do professor informado: "+ id + "é inválido\n");
+    }
+
+    }
 
 }
