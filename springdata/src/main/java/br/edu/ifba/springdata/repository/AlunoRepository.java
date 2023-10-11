dd6f18480c99268ef2ace7fa74103b6d123ada4d
Luana Braga <luanabragagomes9@gmail.com>
2023-10-11 15:09:34 -0300

    busca jpql e native sql

--- springdata/src/main/java/br/edu/ifba/springdata/repository/AlunoRepository.java
+++ springdata/src/main/java/br/edu/ifba/springdata/repository/AlunoRepository.java
@@ -2,6 +2,7 @@ package br.edu.ifba.springdata.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,8 +11,14 @@ import br.edu.ifba.springdata.orm.Aluno;
 @Repository
 public interface AlunoRepository extends CrudRepository<Aluno, Long>{
     //buscar um aluno do banco com o nome
-    List<Aluno> findByNomeStartingWith(String nome);
+     List<Aluno> findByNomeStartingWith(String nome);
      List<Aluno> findByNomeStartingWithAndIdadeLesThanEqua(String nome, Integer idade);
 
+    //  JPQL
+    @Query("SELECT a FROM Aluno a WHERE a.nome like :nome% AND a.idade >= :idade")
+    List<Aluno> findByNomeIdadeIgualOuMaior(String nome, Integer idade);
 
+    //  JPQL
+    @Query("SELECT a FROM Aluno a INNER JOIN a.disciplina d WHERE a.name  like :nomeAluno% AND a.idade >= :idadeAluno AND d.nome like  :nomeDisciplina")
+    List<Aluno> findNomeIdadeIgualOuMaiorMatricula(String nomeAluno, Integer idadeIdade, String nomeDisciplina);
 }
--- springdata/src/main/java/br/edu/ifba/springdata/repository/ProfessorRepository.java
+++ springdata/src/main/java/br/edu/ifba/springdata/repository/ProfessorRepository.java
@@ -1,5 +1,8 @@
 package br.edu.ifba.springdata.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import org.springframework.stereotype.Repository;
@@ -8,5 +11,9 @@ import br.edu.ifba.springdata.orm.Professor;
 
 @Repository
 public interface ProfessorRepository extends CrudRepository<Professor, Long> {
+       //sql nativa
+       @Query(nativeQuery  = true,
+       value = "SELECT * FROM professores  p INNER JOIN disciplina d ON p.id = d.professor_id WHERE p.nome like : nomeProfessor% AND d.nome like :nomeDisciplina %")
+       List<Professor> findProfessorAtribuido(String nomeProfessor, String nomeoDisciplina);
 
 }
